eee728736a20b253c8e8786202135a0f4dae6824
Ian Blenke <ian@blenke.com>
2016-03-10 11:12:30 -0500

    First proposed build of HockeySDK-Mac from private source submodule repo

--- Tools/compile.sh
+++ Tools/compile.sh
@@ -3,6 +3,23 @@ set -xe
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
 
+set -o pipefail
+
+cd submodules/HockeyApp-Mac
+bundle install
+cd Support
+HOCKEYSDK_FRAMEWORK_PATH=$(xcodebuild -project HockeySDK.xcodeproj -scheme "HockeySDK" -configuration Release -destination "platform=OS X" clean && xcodebuild -project HockeySDK.xcodeproj -scheme "HockeySDK" -configuration Release -destination "platform=OS X" | grep touch  |awk '{print $3}')
+
+cd $DIR/..
+
+if [ -d "$HOCKEYSDK_FRAMEWORK_PATH" ] ; then
+  if [ -d HockeySDK.framework ] ; then
+    rm -fr HockeySDK.framework
+  fi
+
+  ln -sf "$HOCKEYSDK_FRAMEWORK_PATH" HockeySDK.framework
+fi
+
 make -j 8
 ninja -C WORK/cmake
 wget http://ciscobinary.openh264.org/libopenh264-1.5.0-osx64.dylib.bz2 
